77e9130fc62cb1644accbe0b2e54a51e57b8cd26
Sergii Bondarenko <sergibondarenko@gmail.com>
2021-09-14 14:42:08 +0200

    Remove pages and Pages.

--- src/pages/HomePage/HomePage.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { WorkSpaceGrid } from './WorkSpaceGrid';
-
-export function HomePage({ ...props }) {
-  return <div data-testid="app-home"><WorkSpaceGrid { ...props } /></div>;
-}

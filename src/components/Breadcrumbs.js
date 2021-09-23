351c3c4bd1bdea81dc9feadb0a3b4e271f284dba
Sergii Bondarenko <sergibondarenko@gmail.com>
2021-09-23 17:29:30 +0200

    Make the breadcrumbs look better.

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Breadcrumbs as MuiBreadcrumbs, Link } from '@material-ui/core';
+import { NavigateNext as NavigateNextIcon } from '@material-ui/icons';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { startCase } from 'lodash';
 
 const LinkRouter = (props) => <Link {...props} component={RouterLink} />
 
-export function BreadcrumbsLinks() {
+export function Breadcrumbs() {
   const { pathname } = useLocation();
 
   if (pathname === '/') {
     return (
-      <LinkRouter color="textPrimary" to="/">
-        home
-      </LinkRouter>
+      <MuiBreadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+        <LinkRouter color="textPrimary" to="/">
+          My Spaces
+        </LinkRouter>
+      </MuiBreadcrumbs>
     );
   }
-  
+
+  let pathnameVisual = pathname.split('/').filter((str) => !!str.length);
+  pathnameVisual[0] = startCase(pathnameVisual[0]);
+  pathnameVisual = pathnameVisual.join(' ');
+
   return (
-    <>
+    <MuiBreadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
       <LinkRouter color="inherit" to="/">
-        home
+        My Spaces
       </LinkRouter>
       {" "}
       <LinkRouter color="textPrimary" to={pathname}>
-        {pathname}
+        {pathnameVisual}
       </LinkRouter>
-    </>
-  );
-}
-
-export function Breadcrumbs() {
-  return (
-    <MuiBreadcrumbs aria-label="breadcrumb">
-      <BreadcrumbsLinks />
     </MuiBreadcrumbs>
   );
 }
